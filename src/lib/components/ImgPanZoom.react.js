391ebb77855836a9a26a50923e2cd1473884a156
Charley Ferrari <charley@charleyferrari.com>
2020-04-14 19:50:48 -0400

    moving working THANKS SCOTT

--- src/lib/components/ImgPanZoom.react.js
+++ src/lib/components/ImgPanZoom.react.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 /**
@@ -16,30 +16,136 @@ export default class ImgPanZoom extends Component {
         gkhead.src = this.props.src;
         let canvas = this.refs.test;
         let ctx = canvas.getContext('2d');
+        this.trackTransforms(ctx);
         gkhead.onload = () => {
-            ctx.drawImage(gkhead,50,50);
+            console.log(this);
+            this.redraw(ctx, canvas, gkhead);
         }
 
-        
+        let lastX = canvas.width / 2, lastY = canvas.height / 2;
 
-        // console.log(ctx);
+        let dragStart, dragged;
 
-        // console.log(canvas);
-        // const context = this.getContext('2d');
-      
-        // const image = new Image();
-        // image.src = "whereever-you-image-url-live.jpg";
-        // image.onload = () => {
-        //   context.drawImage(image, 0, 0, this.canvasA.width, this.canvasA.height);
-        // };
-      }
+        const onMouseDown = evt => {
+            document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+            lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+            lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+            dragStart = ctx.transformedPoint(lastX, lastY);
+            dragged = false;
+            console.log('mousedown');
+        }
+
+        const onMouseMove = evt => {
+            lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+            lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+            dragged = true;
+            if (dragStart) {
+                var pt = ctx.transformedPoint(lastX, lastY);
+                ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y);
+                console.log(this);
+                this.redraw(ctx, canvas, gkhead);
+            }
+            console.log('mousemove');
+        }
+
+        const onMouseUp = evt => {
+            dragStart = null;
+            if (!dragged) zoom(evt.shiftKey ? -1 : 1);
+            console.log('mouseup');
+        }
+
+        canvas.addEventListener('mousedown', onMouseDown, false);
+
+        canvas.addEventListener('mousemove', onMouseMove, false);
+
+        canvas.addEventListener('mouseup', onMouseUp, false);
+
+    }
 
     render() {
-        const {id, height, width, src, setProps} = this.props;
+        const { id, height, width, src, setProps } = this.props;
 
         return (
             <canvas ref={id} width={String(width)} height={String(height)} />
         );
+
+    }
+
+    trackTransforms(ctx) {
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+        var xform = svg.createSVGMatrix();
+        ctx.getTransform = function () { return xform; };
+
+        var savedTransforms = [];
+        var save = ctx.save;
+        ctx.save = function () {
+            savedTransforms.push(xform.translate(0, 0));
+            return save.call(ctx);
+        };
+
+        var restore = ctx.restore;
+        ctx.restore = function () {
+            xform = savedTransforms.pop();
+            return restore.call(ctx);
+        };
+
+        var scale = ctx.scale;
+        ctx.scale = function (sx, sy) {
+            xform = xform.scaleNonUniform(sx, sy);
+            return scale.call(ctx, sx, sy);
+        };
+
+        var rotate = ctx.rotate;
+        ctx.rotate = function (radians) {
+            xform = xform.rotate(radians * 180 / Math.PI);
+            return rotate.call(ctx, radians);
+        };
+
+        var translate = ctx.translate;
+        ctx.translate = function (dx, dy) {
+            xform = xform.translate(dx, dy);
+            return translate.call(ctx, dx, dy);
+        };
+
+        var transform = ctx.transform;
+        ctx.transform = function (a, b, c, d, e, f) {
+            var m2 = svg.createSVGMatrix();
+            m2.a = a; m2.b = b; m2.c = c; m2.d = d; m2.e = e; m2.f = f;
+            xform = xform.multiply(m2);
+            return transform.call(ctx, a, b, c, d, e, f);
+        };
+
+        var setTransform = ctx.setTransform;
+        ctx.setTransform = function (a, b, c, d, e, f) {
+            xform.a = a;
+            xform.b = b;
+            xform.c = c;
+            xform.d = d;
+            xform.e = e;
+            xform.f = f;
+            return setTransform.call(ctx, a, b, c, d, e, f);
+        };
+
+        var pt = svg.createSVGPoint();
+        ctx.transformedPoint = function (x, y) {
+            pt.x = x; pt.y = y;
+            return pt.matrixTransform(xform.inverse());
+        }
+    }
+
+    redraw(ctx, canvas, gkhead) {
+
+        // Clear the entire canvas
+        var p1 = ctx.transformedPoint(0, 0);
+        var p2 = ctx.transformedPoint(canvas.width, canvas.height);
+        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.restore();
+
+        ctx.drawImage(gkhead, 50, 50);
 
     }
 }
